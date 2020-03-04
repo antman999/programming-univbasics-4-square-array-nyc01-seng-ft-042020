7105eb49d897984d8c7561f9998416b9463e4a3b
Anthony mendoza <anthonymendoza9922@gmail.com>
2020-03-04 04:09:12 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,10 @@
-def square_array(array)
-  # your code here
+# i want to pretty much loop while **2 but 
+
+
+def square_array(numbers)
+  return numbers.map {|num| num ** 2}
 end
+
+
+
+
--- spec/square_array_spec.rb
+++ spec/square_array_spec.rb
@@ -6,7 +6,6 @@ describe '#square_array' do
     numbers = [1,2,3]
     expect(numbers).to_not receive(:each)
     expect(numbers).to_not receive(:collect)
-    expect(numbers).to_not receive(:map)
     expect(numbers).to_not receive(:inject)
     square_array(numbers)
   end
