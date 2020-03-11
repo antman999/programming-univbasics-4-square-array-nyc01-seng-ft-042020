8ed070a13420d88844b47ca980e5b28b45644f3c
Anthony Mendoza <anthonymendoza9922@gmail.com>
2020-03-11 03:47:27 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,13 +1,13 @@
 # i want to pretty much loop while **2 but 
 
 def square_array(array)
-  squared = []
-  counter = 0
-  while counter < array.length do
-    squared << (array[counter] ** 2)
-    counter = counter + 1
-  end
-  squared
+index = []
+counter = 0 
+while counter < array.length do 
+  index = (array<<[counter]**2)
+  counter +=1
+end
+index
 end
 
 
